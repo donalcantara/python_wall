96a2431973de2605b3b2f2084c999bff1c5b7ebc
Don Alcantara <don.alcantara@yahoo.com>
2016-09-29 13:08:09 -0700

    added an edit page

--- server.py
+++ server.py
@@ -7,37 +7,6 @@ mysql = MySQLConnector(app, 'wall')
 
 app.secret_key = "ThisIsSecret!"
 
-@app.route('/login', methods=['POST'])
-def login():
-	error = 0
-	temp = request.form['username']
-	username = temp.lower()
-	print username
-	password = request.form['password']
-	if len(username) < 1:
-		flash("Username cannot be empty")
-		error += 1
-	if len(request.form['password']) < 1:
-		flash("Password cannot be empty")
-		error += 1
-	query = "SELECT * FROM users WHERE users.username = :username LIMIT 1"
-	data = {'username': username}
-	user = mysql.query_db(query, data)
-	if error > 0:
-		print error
-		return redirect('/')
-	if len(user) == 0:
-		flash('There is no account with that username!')
-		return redirect('/')
-	if user[0]:
-		if user[0]['password'] == password:
-			session['id'] = user[0]['id']
-			session['username'] = username
-			return redirect('/wall')
-		else:
-			flash("Incorrect username and/or password!")
-			return redirect('/')
-
 @app.route('/register', methods=['POST'])
 def submit():
 	temp = request.form['username']
@@ -98,16 +67,58 @@ def index():
 	users = mysql.query_db(query)
 	return render_template('index.html', users = users)
 
+@app.route('/login', methods=['POST'])
+def login():
+	error = 0
+	temp = request.form['username']
+	username = temp.lower()
+	print username
+	password = request.form['password']
+	if len(username) < 1:
+		flash("Username cannot be empty")
+		error += 1
+	if len(request.form['password']) < 1:
+		flash("Password cannot be empty")
+		error += 1
+	query = "SELECT * FROM users WHERE users.username = :username LIMIT 1"
+	data = {'username': username}
+	user = mysql.query_db(query, data)
+	if error > 0:
+		print error
+		return redirect('/')
+	if len(user) == 0:
+		flash('There is no account with that username!')
+		return redirect('/')
+	if user[0]:
+		if user[0]['password'] == password:
+			session['id'] = user[0]['id']
+			session['username'] = username
+			return redirect('/wall')
+		else:
+			flash("Incorrect username and/or password!")
+			return redirect('/')
+
 @app.route('/wall')
 def wall():
-	posts = mysql.query_db("SELECT posts.content, posts.created_at, users.name, users.username, users.id AS user_id, posts.id AS post_id FROM posts join users ON users.id = posts.user_id ORDER BY posts.created_at DESC")
+	query = "SELECT * FROM users WHERE users.username = :username LIMIT 1"
+	data = {'username': session['username']}
+	users = mysql.query_db(query, data)
 	print session['username']
+	posts = mysql.query_db("SELECT posts.content, posts.created_at, users.name, users.username, users.id AS user_id, posts.id AS post_id FROM posts join users ON users.id = posts.user_id ORDER BY posts.created_at DESC")
 	for post in posts:
 		post['created_at'] = post['created_at'].strftime('%m/%d/%Y %-I:%M:%S %p')
-	comments = mysql.query_db("SELECT * FROM users JOIN comments on users.id = comments.user_id ORDER BY comments.created_at ASC")
+		comments = mysql.query_db("SELECT * FROM users JOIN comments on users.id = comments.user_id ORDER BY comments.created_at ASC")
 	for comment in comments:
 		comment['created_at'] = comment['created_at'].strftime('%m/%d/%Y %-I:%M:%S %p')
 	return render_template('wall.html', posts=posts, comments=comments)
+
+@app.route('/account/<username>', methods=['GET'])
+def show(username): #user_id is the variable that you're passing
+	print username
+	query = "SELECT * FROM users WHERE username = :specific_id" #this is the query you're sending to the database
+	data = {'specific_id': username} # this is the data you're sending to the datbase
+	users = mysql.query_db(query, data) #combine the query with the data and you'll get back data from the database
+	return render_template('edit.html', users=users) #this sends you to the edit.html page with friends being the data you requested
 
 @app.route('/post/<user_id>', methods=['POST'])
 def post(user_id):
@@ -160,24 +171,17 @@ def deletecomment(comment_id):
 	return redirect('/wall')
 
 
-# @app.route('/update/<friend_id>', methods=['POST'])
-# def update(friend_id):
-# 	query = "UPDATE friends SET first_name = :first_name, last_name = :last_name, occupation = :occupation, updated_at = NOW() WHERE id = :id" #the query that you're updating info from the db back to the db
-# 	data = { #this is the data you'll be updating in the db
-# 		'first_name': request.form['first_name'], 
-# 		'last_name':  request.form['last_name'],
-# 		'occupation': request.form['occupation'],
-# 		'id': friend_id
-# 		}
-# 	mysql.query_db(query, data)
-# 	return redirect('/')
-
-# @app.route('/edit/<friend_id>', methods=['POST'])
-# def show(friend_id): #friend_id is the variable that you're passing
-# 	query = "SELECT * FROM friends WHERE id = :specific_id" #this is the query you're sending to the database
-# 	data = {'specific_id': friend_id} # this is the data you're sending to the datbase
-# 	friends = mysql.query_db(query, data) #combine the query with the data and you'll get back data from the database
-# 	return render_template('edit.html', friends=friends) #this sends you to the edit.html page with friends being the data you requested
+@app.route('/update/<user_id>', methods=['POST'])
+def update(user_id):
+	query = "UPDATE users SET name = :name, username = :username, email = :email, updated_at = NOW() WHERE id = :id" #the query that you're updating info from the db back to the db
+	data = { #this is the data you'll be updating in the db
+		'name': request.form['name'], 
+		'username':  request.form['username'],
+		'email': request.form['email'],
+		'id': user_id
+		}
+	mysql.query_db(query, data)
+	return redirect('/wall')
 
 app.run(debug=True)
 
